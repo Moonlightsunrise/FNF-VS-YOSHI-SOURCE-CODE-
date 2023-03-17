be439267b483e111621f6eebde8ca4f60e9bcf74
Moonlightsunrise <128168675+Moonlightsunrise@users.noreply.github.com>
2023-03-17 16:26:31 +0000

    Update LoadingState.hx

--- source/LoadingState.hx
+++ source/LoadingState.hx
@@ -15,6 +15,37 @@ import lime.utils.AssetManifest;
 
 import haxe.io.Path;
 
+
+//if you want to add the vpad to a state
+#if mobile
+addVirtualPad(LEFT_FULL, A_B);
+#end
+
+//if you want it to have a camera
+#if mobile
+addVirtualPadCamera(); // add false into () if isn't the default draw camera...
+#end
+
+//if you want to remove it at some moment use
+#if mobile
+removeVirtualPad();
+#end
+
+//if you want to add the hitbox to a state
+#if mobile
+addHitbox();
+#end
+
+//if you want it to have a camera
+#if mobile
+addHitboxCamera(); // add false into () if isn't the default draw camera...
+#end
+
+//if you want to remove it at some moment use
+#if mobile
+removeHitbox();
+#end
+
 class LoadingState extends MusicBeatState
 {
 	inline static var MIN_TIME = 1.0;
