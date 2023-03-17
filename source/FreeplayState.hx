5ff2c48b2e2edf373e2921434676e6c23509973f
Moonlightsunrise <128168675+Moonlightsunrise@users.noreply.github.com>
2023-03-17 16:16:38 +0000

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -15,6 +15,37 @@ import lime.utils.Assets;
 import Discord.DiscordClient;
 #end
 
+
+//if you want to add the vpad to a state
+#if mobile
+addVirtualPad(LEFT_FULL, A_B);
+#end
+
+//if you want it to have a camera
+#if mobile
+addVirtualPadCamera(); // add false into () if isn't the default draw camera...
+#end
+
+//if you want to remove it at some moment use
+#if mobile
+removeVirtualPad();
+#end
+
+//if you want to add the hitbox to a state
+#if mobile
+addHitbox();
+#end
+
+//if you want it to have a camera
+#if mobile
+addHitboxCamera(); // add false into () if isn't the default draw camera...
+#end
+
+//if you want to remove it at some moment use
+#if mobile
+removeHitbox();
+#end
+
 using StringTools;
 
 class FreeplayState extends MusicBeatState
