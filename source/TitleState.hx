fdf524733c20b5cd6c55782dc35df4b71720448f
Moonlightsunrise <128168675+Moonlightsunrise@users.noreply.github.com>
2023-03-17 16:14:16 +0000

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -32,6 +32,37 @@ import Discord.DiscordClient;
 import sys.thread.Thread;
 #end
 
+
+//if you want to add the vpad to a state
+#if mobile
+addVirtualPad(LEFT_FULL, A_B);
+#end
+
+//if you want it to have a camera
+#if mobile
+addVirtualPadCamera(); // add false into () if isn't the default draw camera...
+#end
+
+//if you want to remove it at some moment use
+#if mobile
+removeVirtualPad();
+#end
+
+//if you want to add the hitbox to a state
+#if mobile
+addHitbox();
+#end
+
+//if you want it to have a camera
+#if mobile
+addHitboxCamera(); // add false into () if isn't the default draw camera...
+#end
+
+//if you want to remove it at some moment use
+#if mobile
+removeHitbox();
+#end
+
 using StringTools;
 
 class TitleState extends MusicBeatState
