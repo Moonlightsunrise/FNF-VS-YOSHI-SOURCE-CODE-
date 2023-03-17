d341b95b46a008e24e65a848013d99ec1ec89b38
Moonlightsunrise <128168675+Moonlightsunrise@users.noreply.github.com>
2023-03-17 16:17:59 +0000

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -19,6 +19,37 @@ import lime.app.Application;
 import Discord.DiscordClient;
 #end
 
+
+//if you want to add the vpad to a state
+#if mobile
+addVirtualPad(LEFT_FULL, A_B);
+#end
+
+//if you want it to have a camera
+#if mobile
+addVirtualPadCamera(); // add false into () if isn't the default draw camera...
+#end
+
+//if you want to remove it at some moment use
+#if mobile
+removeVirtualPad();
+#end
+
+//if you want to add the hitbox to a state
+#if mobile
+addHitbox();
+#end
+
+//if you want it to have a camera
+#if mobile
+addHitboxCamera(); // add false into () if isn't the default draw camera...
+#end
+
+//if you want to remove it at some moment use
+#if mobile
+removeHitbox();
+#end
+
 using StringTools;
 
 class MainMenuState extends MusicBeatState
